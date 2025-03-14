8586201ff5499e75e5a2529223886b479734a409
li <321826612@qq.com>
2025-03-14 23:43:47 +0800

    first commit

---
+++ pay-starter/src/main/java/edu/ncu/pay/demo/starter/Application.java
@@ -0,0 +1,12 @@
+package edu.ncu.pay.demo.starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(scanBasePackages = {"edu.ncu.pay.demo"}, exclude = { DataSourceAutoConfiguration.class })
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
